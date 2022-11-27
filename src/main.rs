c9685040286d2b1222dd5b34971987998bb4073e
Ivan <clicker.heroes.acg@gmail.com>
2022-11-27 03:15:59 +0300

    app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use clap::{App, Arg};
+
 mod converter;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("vtg")
+        .author("Ivan I. <clicker.heroes.acg@gmail.com>")
+        .about("This small cli-tool will help you with conversion video into gif.")
+        .arg(
+            Arg::with_name("origin")
+                .help("original video.")
+                .short("o")
+                .required(true)
+                .takes_value(true)
+        )
+        .get_matches();
+    let origin = matches.value_of("origin")
+        .expect("Error with origin file...");
 }
